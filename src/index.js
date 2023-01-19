4829cd5e8d4a5536e0ab28267ca06b3afb226e5f
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-01-19 15:08:21 -0500

    updated solution with the creation of a form as a separate function

--- src/index.js
+++ src/index.js
@@ -10,77 +10,50 @@ const newRamenForm = document.querySelector('#new-ramen');
 
 
 //Create a fetch request
-
 fetch('http://localhost:3000/ramens')
-  .then(response => response.json())
-  .then(data => displayAllRamen(data));
+    .then(response => response.json())
+    .then(data => displayAllRamen(data))
+    .then(data => setUpRamenForm(data));
 
 
 //create a function that loops through the ramen list and displays them
 function displayAllRamen(ramenList) {
-  ramenList.forEach(data => displaySingularRamen(data));
+    ramenList.forEach(data => displaySingularRamen(data));
 }
-
-// displayAllRamen(ramenList);
-
-
 //create a function that  shows one ramen object
 
 function displaySingularRamen(ramenObj) {
-  const ramenImage = document.createElement('img');
-  ramenImage.src = ramenObj.image;
-  ramenImage.alt = `${ramenObj.name} image`
-  ramenImage.title = `${ramenObj.name} image`
+    const ramenImage = document.createElement('img');
+    ramenImage.src = ramenObj.image;
+    ramenImage.alt = `${ramenObj.name} image`
+    ramenImage.title = `${ramenObj.name} image`
 
-  ramenImage.addEventListener('click', (e) => {
-    // console.log('The click event works')
-    detailImage.src = ramenObj.image
-    ramenName.textContent = ramenObj.name
-    restaurant.textContent = ramenObj.restaurant
-    ratingDisplay.textContent = ramenObj.rating
-    commentDisplay.textContent = ramenObj.comment
+    ramenImage.addEventListener('click', (e) => {
+        // console.log('The click event works')
+        detailImage.src = ramenObj.image
+        ramenName.textContent = ramenObj.name
+        restaurant.textContent = ramenObj.restaurant
+        ratingDisplay.textContent = ramenObj.rating
+        commentDisplay.textContent = ramenObj.comment
 
-  })
-  newRamenForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(e.target)
-    // console.log(e.target['new-name'])
-    const newRamenObj = {
-      name: e.target.name.value,
-      restaurant: e.target.restaurant.value,
-      image: e.target.image.value,
-      rating: e.target.rating.value,
-      comment: e.target["new-comment"].value
-    }
-    displaySingularRamen(newRamenObj)
+    })
 
-  })
-  ramenMenu.append(ramenImage)
-
-  //create a ramen form
-//Add e.prevent to stop form refreshing
-// create an new ramen object
-//target the ramen object using e.target method
-//pass new ramen object as an argument
-
+    ramenMenu.append(ramenImage)
 }
 
-// displaySingularRamen(newRamenObj)
+function setUpRamenForm() {
+    newRamenForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        console.log(e.target)
+        // console.log(e.target['new-name'])
+        const newRamenObj = {
+            name: e.target.name.value,
+            restaurant: e.target.restaurant.value,
+            image: e.target.image.value,
+            rating: e.target.rating.value,
+            comment: e.target["new-comment"].value
+        }
+        displaySingularRamen(newRamenObj)
 
-
-
-
-
-
-
-
-//create a ramen form
-//Add e.prevent to stop form refreshing
-// create an new ramen object
-//target the ramen object using e.target method
-//pass new ramen object as an argument
-
-
-
-
-
+    })
+}
